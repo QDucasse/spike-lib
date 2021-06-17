4a20a4b01d5c5b8f0c0207153278e724f228070e
Quentin DUCASSE <quentin.ducasse@ensta-bretagne.org>
2021-06-17 13:31:19 +0200

    Refactoring to check for empty C wrapper

--- spikelib.cpp
+++ spikelib.cpp
@@ -10,10 +10,10 @@
 #define EXPORT  __attribute__ ((visibility ("default")))
 
 extern "C" {
-    void* doInitialize_sim();
-    // void* initialize_sim();
-    // uint32_t read_register(void* sim, int nb);
-    // void write_register(void* sim, int nb, uint32_t value);
+    // void* doInitialize_sim(); // when using C wrapper
+    void* initialize_sim();
+    uint32_t read_register(void* sim, int nb);
+    void write_register(void* sim, int nb, uint32_t value);
 }
 
 std::vector<std::pair<reg_t, mem_t*>> initialize_mems(int base, int size, char* contents) {
@@ -89,19 +89,19 @@ void* doInitialize_sim(){
 //       PHARO API WRAPPERS
 // =================================
 
-// EXPORT void* initialize_sim(){
-//     return doInitialize_sim();
-// }
+EXPORT void* initialize_sim(){
+    return doInitialize_sim();
+}
 
-// uint32_t read_register(void* sim, int nb) {
-//    sim_t* real_sim = (sim_t*) sim;
-//    return real_sim->get_core(0)->get_state()->XPR[nb];
-// }
+uint32_t read_register(void* sim, int nb) {
+   sim_t* real_sim = (sim_t*) sim;
+   return real_sim->get_core(0)->get_state()->XPR[nb];
+}
 
-// void write_register(void* sim, int nb, uint32_t value) {
-//    sim_t* real_sim = (sim_t*) sim;
-//    real_sim->get_core(0)->get_state()->XPR.write(nb, value);
-// }
+void write_register(void* sim, int nb, uint32_t value) {
+   sim_t* real_sim = (sim_t*) sim;
+   real_sim->get_core(0)->get_state()->XPR.write(nb, value);
+}
 
 
 
